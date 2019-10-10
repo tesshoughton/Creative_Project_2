cb70c63687d78606b06f3db6161154d0b8507321
EC2 Default User <ec2-user@ip-172-31-33-206.us-east-2.compute.internal>
2019-10-10 04:45:43 +0000

    added most functionality, core design

--- script.js
+++ script.js
@@ -11,13 +11,26 @@ document.getElementById("searchSubmit").addEventListener("click", function(event
       return response.json();
   }).then(function(json) {
       console.log(json);
-      let results="<h2>Search Results</h2>";
-      for (let i=0; i < json.artObjects.length; i++) {
-          console.log(json.artObjects[i].title);
-          results += "<p>";
-          results += json.artObjects[i].title;
-          results += "</p>";
-          // From here, we just need to choose what to display and make it look nice
+      let results = "";
+      if (json.artObjects.length === 0) {
+        results = '<h3>Sorry, no results matched your search.</h3>';
+      } else {
+        results='<table id="my_table"><tr id="table_head"><th scope="col">Title</th><th scope="col">Work</th><th scope="col">Artist</th></tr>';
+        for (let i=0; i < json.artObjects.length; i++) {
+          if (i % 2 == 1) {
+            results += '<tr class="evens">';
+          } else {
+            results += '<tr class="odds">';
+          }
+          results += '<th scope="row">' + json.artObjects[i].title + '</th>';
+          if (json.artObjects[i].hasImage === false) {
+            results += "<td>No image available</td>";
+          } else {
+            results += '<td><img src="' + json.artObjects[i].webImage.url.replace("s0", "s256") + '"/></td>';
+          }
+          results += '<td>' + json.artObjects[i].principalOrFirstMaker + '</td></tr>';
+        }
+        results += "</table>"
       }
       document.getElementById("searchResults").innerHTML = results;
   });
